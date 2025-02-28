8589673db02583e84b66185ebeed63138d99bb1b
huaguanzhong <1994868045@qq.com>
2025-02-28 22:42:33 +0800

    路由功能配置

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,25 +1,34 @@
 // router.js
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import AppLayout from '../components/layout/header.vue'
-import Admin from '../views/administration.vue'
-import Analyze from '../views/analyze.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
-Vue.use(VueRouter)
+
+
+
+import Home from '../views/system/index.vue'
+import introduce from '../views/system/introduce.vue'
+
+import Admin from '../views/system/administration.vue'
+import Analyze from '../views/system/analyze.vue'
+import personal from '../views/system/personalCenter.vue'
+
 
 const routes = [
-  {
-    path: '/',
-    component: AppLayout, // 父路由使用布局组件
-    children: [
-      { path: '/admin', component: Admin  },
-      { path: '/analyze', component: Analyze }
-    ]
-  }
+  // {
+  //   path: '/',
+  //   component: AppLayout, // 父路由使用布局组件
+  //   children: [
+      { path: '/home', component: Home},
+      { path: '/admin', component: Admin },
+      { path: '/analyze', component: Analyze },
+      { path: '/introduce', component: introduce },
+      { path: '/personal', component: personal}
+
+  //   ]
+  // }
 ]
 
-const router = new VueRouter({
-  mode: 'history',
+const router = createRouter({
+  history:createWebHistory(),
   routes
 })
 
