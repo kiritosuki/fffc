1afb02d28000b8e2a5d9a25f7edf356df433cb4c
huaguanzhong <1994868045@qq.com>
2025-03-03 18:57:15 +0800

    删除功能修改

--- vue-project/src/views/system/index.vue
+++ vue-project/src/views/system/index.vue
@@ -23,8 +23,11 @@ export default {
         console.log(res);
       })
       ids = [];
+    },
+    buttontask() {
+      this.CheckPatient("","","","",1,10)
     }
-    
+
 
   },
 }
@@ -34,41 +37,47 @@ export default {
 
 
 <template>
-    
-
-    <h1>查询功能
-</h1>
-    <input type="text" v-model="name" placeholder="name"><br>
-    <input type="text" v-model="gender" placeholder="gender"><br>
-    <input type="text" v-model="begin" placeholder="begin"><br>
-    <input type="text" v-model="end" placeholder="end"><br>
-    <input type="text" v-model="page" placeholder="page"><br>
-    <input type="text" v-model="pageSize" placeholder="pageSize"><br>
-    
-    <button @click="CheckPatient(name, gender, begin, end, page, pageSize)">查询
-    </button><br>
 
 
+  <h1>查询功能
+  </h1>
+  <input type="text" v-model="name" placeholder="name"><br>
+  <input type="text" v-model="gender" placeholder="gender"><br>
+  <input type="text" v-model="begin" placeholder="begin"><br>
+  <input type="text" v-model="end" placeholder="end"><br>
+  <input type="text" v-model="page" placeholder="page"><br>
+  <input type="text" v-model="pageSize" placeholder="pageSize"><br>
+
+  <button @click="CheckPatient(name, gender, begin, end, page, pageSize)">查询
+  </button><br>
 
 
 
 
 
-    <h1>总览与删除功能</h1>
-
-<button @click="CheckPatient()">查询全部</button>
-
-<h1>查询全部并显示</h1>
-
-<div v-for="item in rows" :key="item.id">
-<input type="checkbox" :id="item.id" :value="item.id" v-model="deleteIds">
-<span>    {{ item.name }}         {{ item.gender ? "女" : "男" }}        {{ item.createTime }}      {{ item.updataTime }}</span> 
-
-</div>
 
 
+  <h1>总览与删除功能</h1>
 
-<button @click="DelPatient(deleteIds)">一键删除</button>
+  <button @click="buttontask()">查询全部</button>
+
+  <h1>查询全部并显示</h1>
+
+  <div v-for="item in rows" :key="item.id" class="patient">
+    <input type="checkbox" :id="item.id" :value="item.id" v-model="deleteIds">
+    <span> {{ item.name }} {{ item.gender ? "女" : "男" }} {{ item.createTime }} {{ item.updataTime }}</span>
+
+  </div>
+
+
+
+  <button @click="DelPatient(deleteIds)">一键删除</button>
 
 
 </template>
+<style>
+.patient {
+  margin: 10px;
+  display: block;
+}
+</style>
