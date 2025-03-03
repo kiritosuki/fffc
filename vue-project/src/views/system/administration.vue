46a13db28a278bdb7a50fb28af972a8111a5ac0e
qiqiyiyiguo <3370256643@qq.com>
2025-03-03 16:21:16 +0800

    暂存添加病人的弹窗（未完成）

--- vue-project/src/views/system/administration.vue
+++ vue-project/src/views/system/administration.vue
@@ -1,3 +1,25 @@
+<!-- 在病例管理页面 -->
 <template>
-    用户
-</template>
+    <div class="patient-management">
+      <el-button 
+        type="primary" 
+        icon="Plus"
+        @click="openAddDialog"
+      >
+        新增病例
+      </el-button>
+      
+      <add-patient-dialog ref="addDialogRef" />
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue'
+  import AddPatientDialog from '@/components/patient/AddPatientDialog.vue'
+  
+  const addDialogRef = ref(null)
+  
+  const openAddDialog = () => {
+    addDialogRef.value.handleOpen()
+  }
+  </script>
