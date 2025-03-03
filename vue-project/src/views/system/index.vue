39885db5020916d08252acd9aca62f80f13c1e6f
kiritosuki <3322640054@qq.com>
2025-03-03 22:05:06 +0800

    hgzdeletefinished

--- vue-project/src/views/system/index.vue
+++ vue-project/src/views/system/index.vue
@@ -1,4 +1,9 @@
 <script>
+import * as patient from '../../api/patients'
+
+
+let data;
+
 export default {
   data() {
     return {
@@ -16,21 +21,31 @@ export default {
     CheckPatient(name, gender, begin, end, page = 1, pageSize = 10) {
       this.$api.CheckPatient(this.name, this.gender, this.begin, this.end, this.page, this.pageSize).then(res => {
         console.log(res);
-      })
+
+      
+    })
     },
     DelPatient(ids) {
       this.$api.DelPatient(ids).then(res => {
         console.log(res);
+        this.buttontask()
       })
       ids = [];
     },
     buttontask() {
-      this.CheckPatient("","","","",1,10)
-    }
+       this.$api.CheckPatient("","","","",1,10).then(res => {
+        let data =  res.data.data.rows
+        this.rows = data
+       console.log(data)
+      },)
+
+
 
 
   },
+},
 }
+   
 </script>
 
 
@@ -61,17 +76,17 @@ export default {
 
   <button @click="buttontask()">查询全部</button>
 
-  <h1>查询全部并显示</h1>
+
 
   <div v-for="item in rows" :key="item.id" class="patient">
     <input type="checkbox" :id="item.id" :value="item.id" v-model="deleteIds">
-    <span> {{ item.name }} {{ item.gender ? "女" : "男" }} {{ item.createTime }} {{ item.updataTime }}</span>
+    <span> {{ item.name }}       {{ item.gender ? "女" : "男" }}      {{ item.createTime }}       {{ item.updataTime }}</span>
 
   </div>
 
+<h1>查询全部并显示</h1>
 
-
-  <button @click="DelPatient(deleteIds)">一键删除</button>
+    <button @click="DelPatient(deleteIds)">一键删除</button>
 
 
 </template>
