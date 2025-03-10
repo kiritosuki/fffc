f674484b41b71836c3b29f6d9ce8a3114d968dbc
huaguanzhong <1994868045@qq.com>
2025-03-10 15:36:47 +0800

    get

--- vue-project/src/views/system/indexResult.vue
+++ vue-project/src/views/system/indexResult.vue
@@ -18,11 +18,21 @@ const rightImg = ref('')
 const leftIllnessList = ref([])
 const rightIllnessList = ref([])
 
+// 是否是正常的
+const ifLeftNomal = ref(false)
+const ifRightNomal = ref(false)
+
+// 其他异常病症
 const leftOtherIllness= ref('')
 const rightOtherIllness= ref('')
 
+// 是否有输入栏
 const leftinput = ref(false)
 const rightinput = ref(false)
+
+// 诊断关键字
+const leftDiag = ref('')
+const rightDiag = ref('')
 
 // 输入栏内容初始化
 const resInfo = ref('')
@@ -30,25 +40,6 @@ const resInfo = ref('')
 // 提交按钮加载控制初始化
 const submitting = ref(false)
 
-// 判断是否需要显示其他病症的输入栏
-watch(() => leftIllnessList.value, (newValue, oldValue) => {
-  if (newValue.includes('8')) {
-    leftinput.value = true
-  } else {
-    leftinput.value = false
-  }
-},
-{ immediate: true }
-)
-watch(() => rightIllnessList.value, (newValue, oldValue) => {
-  if (newValue.includes('8')) {
-    rightinput.value = true
-  } else {
-    rightinput.value = false
-  }
-},
-{ immediate: true }
-)
 
 
 
@@ -97,7 +88,7 @@ watch(() => rightIllnessList.value, (newValue, oldValue) => {
     leftIllnessList.value = response.data.data.leftStatusIllList
     rightIllnessList.value = response.data.data.rightStatusIllList
 
-    // 判断是否需要显示其他病症的输入栏
+    // 判断是否需要显示其他异常病症的输入栏
     // leftinput =ref(leftIllnessList.includes('8'))
     // rightinput =ref(rightIllnessList.includes('8'))
 
@@ -121,6 +112,22 @@ onMounted(async () => {
       leftIllnessList.value = response.data.leftStatusIllList
       rightIllnessList.value = response.data.rightStatusIllList
 
+      if (leftIllnessList.value.includes('8')){
+        leftinput.value = true
+      } else {
+        leftinput.value = false
+      }
+      if (rightIllnessList.value.includes('8')){
+        rightinput.value = true
+      } else {
+        rightinput.value = false
+      }
+      
+
+
+      leftDiag.value = response.data.leftDiag
+      rightDiag.value = response.data.rightDiag
+
       resInfo.value = response.data.resInfo
     } else {
       console.error("后端返回的 code 不是 1:", response?.data)
@@ -129,6 +136,76 @@ onMounted(async () => {
     console.error("请求失败:", error)
   }
 })
+
+
+// 判断是否需要显示其他异常病症的输入栏
+watch(() => leftIllnessList.value, (newValue, oldValue) => {
+  if (newValue.includes('8')) {
+    leftinput.value = true
+  } else {
+    leftinput.value = false
+  }
+},
+{ immediate: true }
+)
+watch(() => rightIllnessList.value, (newValue, oldValue) => {
+  if (newValue.includes('8')) {
+    rightinput.value = true
+  } else {
+    rightinput.value = false
+  }
+},
+{ immediate: true }
+)
+
+watch(() => leftIllnessList.value, (newValue,oldValue) => {
+  if (newValue.includes('1')) {
+        // 如果选中了“正常”，则清空其他选项
+        // leftIllnessList.value = ['1'];
+        ifLeftNomal.value = true
+      } else {
+        ifLeftNomal.value = false
+      }
+    },
+    { immediate: true }
+);
+
+watch(() => rightIllnessList.value, (newValue,oldValue) => {
+      if (newValue.includes('1')) {
+        // 如果选中了“正常”，则清空其他选项
+        // rightIllnessList.value = ['1'];
+        ifRightNomal.value = true
+      } else {
+        ifRightNomal.value = false
+      }
+    },
+    { immediate: true }
+);
+
+// const handleLeftIllnessList = () => {
+//   if (leftIllnessList.value.includes('1')) {
+//     // 如果选中了“正常”，则清空其他选项
+//     // leftIllnessList.value = ['1'];
+//     ifLeftNomal.value = true
+//   } else {
+//     ifLeftNomal.value = false
+//   }
+// }
+// const handleRightIllnessList = (e) => {
+//   if (rightIllnessList.value.includes('1')) {
+//     // 如果选中了“正常”，则清空其他选项
+//     // rightIllnessList.value = ['1'];
+//     ifRightNomal.value = true
+//   } else {
+//     ifRightNomal.value = false
+//   }
+// }
+
+
+
+
+
+
 
 
 
@@ -152,24 +229,25 @@ const handleFinalResult = () => {
         <p>左眼：</p>
         <img :src="leftImg" alt="左眼图片损毁">
         <p>潜在病症：</p>
-        <input type="checkbox" name="leftIllnessList" value="1" id="1" v-model="leftIllnessList">
-        <label for="1">1号病症</label><br>
-        <input type="checkbox" name="leftIllnessList" value="2" id="2" v-model="leftIllnessList">
-        <label for="2">2号病症</label><br>
-        <input type="checkbox" name="leftIllnessList" value="3" id="3" v-model="leftIllnessList">
-        <label for="3">3号病症</label><br>
-        <input type="checkbox" name="leftIllnessList" value="4" id="4" v-model="leftIllnessList">
-        <label for="4">4号病症</label><br>
-        <input type="checkbox" name="leftIllnessList" value="5" id="5" v-model="leftIllnessList">
-        <label for="5">5号病症</label><br>
-        <input type="checkbox" name="leftIllnessList" value="6" id="6" v-model="leftIllnessList">
-        <label for="6">6号病症</label><br>
-        <input type="checkbox" name="leftIllnessList" value="7" id="7" v-model="leftIllnessList">
-        <label for="7">7号病症</label><br>
-        <input type="checkbox" name="leftIllnessList" value="8" id="8" v-model="leftIllnessList">
-        <label for="8">其他</label><br>
+        <input type="checkbox" name="leftIllnessList" value='1' id="1" v-model="leftIllnessList" @click="handleLeftIllnessList">
+        <label for="1">正常</label><br>
+        <input type="checkbox" name="leftIllnessList" :disabled="ifLeftNomal" value='2' id="2" v-model="leftIllnessList">
+        <label for="2">糖尿病</label><br>
+        <input type="checkbox" name="leftIllnessList" :disabled="ifLeftNomal" value='3' id="3" v-model="leftIllnessList">
+        <label for="3">青光眼</label><br>
+        <input type="checkbox" name="leftIllnessList" :disabled="ifLeftNomal" value='4' id="4" v-model="leftIllnessList">
+        <label for="4">白内障</label><br>
+        <input type="checkbox" name="leftIllnessList" :disabled="ifLeftNomal" value='5' id="5" v-model="leftIllnessList">
+        <label for="5">AMD</label><br>
+        <input type="checkbox" name="leftIllnessList" :disabled="ifLeftNomal" value='6' id="6" v-model="leftIllnessList">
+        <label for="6">高血压</label><br>
+        <input type="checkbox" name="leftIllnessList" :disabled="ifLeftNomal" value='7' id="7" v-model="leftIllnessList">
+        <label for="7">近视</label><br>
+        <input type="checkbox" name="leftIllnessList" :disabled="ifLeftNomal" value='8' id="8" v-model="leftIllnessList">
+        <label for="8">其他异常</label><br>
+        <p>{{ leftDiag }}</p>
         <div id="lefInput">
-        <el-input type="textarea" :rows="3" placeholder="请输入其他病症" v-model="leftOtherIllness" v-if="leftinput">
+        <el-input type="textarea" :rows="3" placeholder="请输入其他异常病症" v-model="leftOtherIllness" v-show="leftinput">
         </el-input>
       </div>
 </div>
@@ -180,24 +258,25 @@ const handleFinalResult = () => {
         <p>右眼：</p>
         <img :src="rightImg" alt="右眼图片损毁">
         <p>潜在病症：</p>
-        <input type="checkbox" name="rightIllnessList" value="1" id="9" v-model="rightIllnessList">
-        <label for="9">1号病症</label><br>
-        <input type="checkbox" name="rightIllnessList" value="2" id="10" v-model="rightIllnessList">
-        <label for="10">2号病症</label><br>
-        <input type="checkbox" name="rightIllnessList" value="3" id="11" v-model="rightIllnessList">
-        <label for="11">3号病症</label><br>
-        <input type="checkbox" name="rightIllnessList" value="4" id="12" v-model="rightIllnessList">
-        <label for="12">4号病症</label><br>
-        <input type="checkbox" name="rightIllnessList" value="5" id="13" v-model="rightIllnessList">
-        <label for="13">5号病症</label><br>
-        <input type="checkbox" name="rightIllnessList" value="6" id="14" v-model="rightIllnessList">
-        <label for="14">6号病症</label><br>
-        <input type="checkbox" name="rightIllnessList" value="7" id="15" v-model="rightIllnessList">
-        <label for="15">7号病症</label><br>
-        <input type="checkbox" name="rightIllnessList" value="8" id="16" v-model="rightIllnessList">
-        <label for="16">其他</label><br>
+        <input type="checkbox" name="rightIllnessList" value='1' id="9" v-model="rightIllnessList" @click="handleRightIllnessList">
+        <label for="9">正常</label><br>
+        <input type="checkbox" name="rightIllnessList" :disabled="ifRightNomal" value='2' id="10" v-model="rightIllnessList">
+        <label for="10">糖尿病</label><br>
+        <input type="checkbox" name="rightIllnessList" :disabled="ifRightNomal" value='3' id="11" v-model="rightIllnessList">
+        <label for="11">青光眼</label><br>
+        <input type="checkbox" name="rightIllnessList" :disabled="ifRightNomal" value='4' id="12" v-model="rightIllnessList">
+        <label for="12">白内障</label><br>
+        <input type="checkbox" name="rightIllnessList" :disabled="ifRightNomal" value='5' id="13" v-model="rightIllnessList">
+        <label for="13">AMD</label><br>
+        <input type="checkbox" name="rightIllnessList" :disabled="ifRightNomal" value='6' id="14" v-model="rightIllnessList">
+        <label for="14">高血压</label><br>
+        <input type="checkbox" name="rightIllnessList" :disabled="ifRightNomal" value='7' id="15" v-model="rightIllnessList">
+        <label for="15">近视</label><br>
+        <input type="checkbox" name="rightIllnessList" :disabled="ifRightNomal" value='8' id="16" v-model="rightIllnessList">
+        <label for="16">其他异常</label><br>
+        <p>{{ rightDiag }}</p>
         <div id="rigInput">
-        <el-input type="textarea" :rows="3" placeholder="请输入其他病症" v-model="rightOtherIllness" v-if="rightinput">
+        <el-input type="textarea" :rows="3" placeholder="请输入其他异常病症" v-model="rightOtherIllness" v-show="rightinput">
         </el-input></div>
       </div>
     </div>
