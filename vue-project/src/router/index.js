f9b66f391790463910bc85ef96d44cd207dc9d70
huaguanzhong <1994868045@qq.com>
2025-03-21 17:32:24 +0800

    初始化历史记录页面，加入路由

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -12,7 +12,7 @@ import Admin from '../views/system/administration.vue'
 import Analyze from '../views/system/analyze.vue'
 import personal from '../views/system/personalCenter.vue'
 import newRecord from '../views/system/newRecord.vue'
-
+import history from '../views/system/history.vue'
 
 const routes = [
   {
@@ -25,7 +25,8 @@ const routes = [
       { path: '/analyze', component: Analyze },
       { path: '/introduce', component: introduce },
       { path: '/personal', component: personal},
-      { path: '/patients/info', component: newRecord,props: (route) => ({ id: route.query.id })}
+      { path: '/patients/info', component: newRecord,props: (route) => ({ id: route.query.id })},
+      { path: '/patients/history', component: history, props: (route) => ({ id: route.query.id })}
     ]
   }
 ]
---
+++ vue-project/src/views/system/history.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const historyList = ref([])
+
+const selectedListChange = (() => {
+
+})
+
+
+onMounted(() => {
+
+})
+</script>
+
+
+<template>
+<el-collapse v-model="historyList" @change="selectedListChange">
+  <el-collapse-item title="一致性 Consistency" name="1">
+
+  </el-collapse-item>
+  <el-collapse-item title="反馈 Feedback" name="2">
+
+  </el-collapse-item>
+  <el-collapse-item title="效率 Efficiency" name="3">
+
+  </el-collapse-item>
+  <el-collapse-item title="可控 Controllability" name="4">
+
+  </el-collapse-item>
+</el-collapse>
+</template>
