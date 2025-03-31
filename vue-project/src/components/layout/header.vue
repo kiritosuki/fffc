90c9e31f74ff32a54b534436594a4f925f02b211
huaguanzhong <1994868045@qq.com>
2025-03-31 13:52:40 +0800

    修改部分header,修改index.html禁用了全局浮动

--- vue-project/src/components/layout/header.vue
+++ vue-project/src/components/layout/header.vue
@@ -1,7 +1,13 @@
 <script setup>
-import { useRoute } from 'vue-router';
+import { useRouter } from 'vue-router';
 
-const route = useRoute();
+const router = useRouter();
+
+
+const routerPush = (path) => {
+  router.push(path);
+}
+
 </script>
 
 
@@ -12,60 +18,106 @@ const route = useRoute();
 
 
 <template>
-    <div class="app-layout">
-      <!-- 固定页眉 -->
-      <header class="header">Header</header>
-  
-      <!-- 固定侧边栏 -->
-      <aside class="sidebar">
-        <router-link to="/home">主页</router-link><br>
-        <router-link to="/homeResult">主页二</router-link><br>
-        <router-link to="/admin">管理</router-link><br>
-        <router-link to="/analyze">分析</router-link><br>
-        <router-link to="/introduce">介绍</router-link><br>
-        <router-link to="/personal">个人</router-link><br>
-        <router-link to="/patients/history">历史记录</router-link><br>
-        <router-link to="/aLotOfPictures">大量图片上传</router-link>
-      </aside>
-  
-      <!-- 动态内容区域 -->
-      <main class="content">
-      <router-view :key="route.fullPath" />
-    </main>
-    </div>
-  </template>
-  
+  <div class="app-layout">
+    <!-- 固定侧边栏 -->
+    <aside class="sidebar">
+      <div id="topContainer">
+      <div ><span id="top">NADVS</span><button class="butTop">回</button></div>
+      </div>
+      <button class="butRouter" @click="routerPush('/home')">主页</button><br>
+      <button class="butRouter" @click="routerPush('/homeResult')">主页二</button><br>
+      <button class="butRouter" @click="routerPush('/admin')">管理</button><br>
+      <button class="butRouter" @click="routerPush('/analyze')">分析</button><br>
+      <button class="butRouter" @click="routerPush('/introduce')">介绍</button><br>
+      <button class="butRouter" @click="routerPush('/personal')">个人</button><br>
+      <button class="butRouter" @click="routerPush('/patients/history')">历史记录</button><br>
+      <button class="butRouter" @click="routerPush('/aLotOfPictures')">大量图片上传</button>
+    </aside>
+
+    <!-- 动态内容区域 -->
+    <main class="content">
+    <router-view :key="router.fullPath" />
+  </main>
+  </div>
+</template>
+
+<style scoped>
+.app-layout {
+  display: grid;
+  grid-template-areas:
+    "sidebar content";
+  grid-template-columns: 220px 1fr;
+  height: 100vh;
+  width: 100vw;
+  margin: -8px;
+  padding: 0;
+}
+
+.sidebar {
+  grid-area: sidebar;
+  background: #f5f5f5;
+  position: absolute;
+  width: 200px;
+  padding: 1rem;
+  height: 100vh;
+  overflow-y: auto;
+}
+
+.content {
+  grid-area: content;
+  padding: 1rem;
+  height: 100vh;
+  overflow: auto;
+}
+
+#topContainer {
+  display: inline-block;
+  width: 100%;
+  margin: 0px, 0px, 10px, 0px;
+}
+
+#topContainer > div {
+  display: grid;
+  grid-template-columns: auto;
+  grid-template-rows: 1fr 1fr;
+  width: 100%;
+  height: auto;
+
+}
+
+#top {
+  display: inline-block;
+  font-size: 32px;
+  font-family: 'Courier New', Courier, monospace;
+}
+
+#topbut {
+
+}
+
+/* #topContainer > div {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: auto;
+
+}
+
+#top {
+  display: inline-block;
+  font-size: 32px;
+  font-family: 'Courier New', Courier, monospace;
+}
+
+#topbut {
+flex: ;
+
+} */
 
 
-
-  <style scoped>
-  .app-layout {
-    display: grid;
-    grid-template-areas:
-      "header header"
-      "sidebar content";
-    grid-template-rows: auto 1fr;
-    grid-template-columns: 200px 1fr;
-    height: 100vh;
-    width: 100vw;
-  }
-  
-  .header {
-    grid-area: header;
-    background: #333;
-    color: white;
-    padding: 1rem;
-  }
-  
-  .sidebar {
-    grid-area: sidebar;
-    background: #f5f5f5;
-    padding: 1rem;
-  }
-  
-  .content {
-    grid-area: content;
-    padding: 1rem;
-    overflow: auto;
-  }
-  </style>
+.butRouter {
+  margin-bottom: 0.5rem;
+  width: 100%;
+}
+</style>
