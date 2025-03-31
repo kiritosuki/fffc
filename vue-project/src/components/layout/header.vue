10deff219adc798103b8f3c8ac02eb1531b56502
huaguanzhong <1994868045@qq.com>
2025-03-31 14:51:46 +0800

    初步构局

--- vue-project/src/components/layout/header.vue
+++ vue-project/src/components/layout/header.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { useRouter } from 'vue-router';
+import { CaretLeft } from '@element-plus/icons-vue'
 
 const router = useRouter();
 
@@ -22,8 +23,12 @@ const routerPush = (path) => {
     <!-- 固定侧边栏 -->
     <aside class="sidebar">
       <div id="topContainer">
-      <div ><span id="top">NADVS</span><button class="butTop">回</button></div>
+      <span id="top">Auris Glow</span><el-button id="butTop"><el-icon :size="28"><CaretLeft /></el-icon></el-button>
       </div>
+      <!-- <div id="topContainer">
+      <div><span id="top">Auris Glow</span></div><button class="butTop">回</button>
+      </div> -->
+      <div>
       <button class="butRouter" @click="routerPush('/home')">主页</button><br>
       <button class="butRouter" @click="routerPush('/homeResult')">主页二</button><br>
       <button class="butRouter" @click="routerPush('/admin')">管理</button><br>
@@ -32,6 +37,7 @@ const routerPush = (path) => {
       <button class="butRouter" @click="routerPush('/personal')">个人</button><br>
       <button class="butRouter" @click="routerPush('/patients/history')">历史记录</button><br>
       <button class="butRouter" @click="routerPush('/aLotOfPictures')">大量图片上传</button>
+    </div>
     </aside>
 
     <!-- 动态内容区域 -->
@@ -46,7 +52,7 @@ const routerPush = (path) => {
   display: grid;
   grid-template-areas:
     "sidebar content";
-  grid-template-columns: 220px 1fr;
+  grid-template-columns: 235px 1fr;
   height: 100vh;
   width: 100vw;
   margin: -8px;
@@ -55,7 +61,7 @@ const routerPush = (path) => {
 
 .sidebar {
   grid-area: sidebar;
-  background: #f5f5f5;
+  background: rgb(249, 251, 255);
   position: absolute;
   width: 200px;
   padding: 1rem;
@@ -70,28 +76,25 @@ const routerPush = (path) => {
   overflow: auto;
 }
 
-#topContainer {
+#topContainer{
   display: inline-block;
   width: 100%;
-  margin: 0px, 0px, 10px, 0px;
-}
-
-#topContainer > div {
-  display: grid;
-  grid-template-columns: auto;
-  grid-template-rows: 1fr 1fr;
+  margin-bottom: 25px;
   width: 100%;
   height: auto;
-
 }
+
 
 #top {
   display: inline-block;
-  font-size: 32px;
+  font-size: 25px;
   font-family: 'Courier New', Courier, monospace;
 }
 
-#topbut {
+#butTop {
+  width: 18px;
+  margin-left: 17px;
+  background-color: rgb(249, 251, 255);
 
 }
 
@@ -119,5 +122,9 @@ flex: ;
 .butRouter {
   margin-bottom: 0.5rem;
   width: 100%;
+  background-color: none;
+  height: 35px;
 }
+
+
 </style>
